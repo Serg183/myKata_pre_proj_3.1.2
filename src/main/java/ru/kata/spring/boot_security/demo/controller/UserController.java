2d758b7329a632925017f11eefab5f657f4e36f8
Serg183 <105773915+Serg183@users.noreply.github.com>
2022-08-07 02:30:11 +1000

    Update UserController.java

--- src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -40,10 +40,17 @@ public class UserController {
         return "redirect:/users";
     }
 
-    @GetMapping("admin/delete/{id}")
+    @DeleteMapping("/delete/{id}")
     public String deleteUser(@PathVariable("id") Long id) {
-        userService.removeUser(id);
-        return "redirect:/users";
+        try {
+            User user = userService.getUserById(id);
+            if(user != null) {
+                userService.removeUser(id);
+            }
+            return "redirect:/users";
+        } catch (Exception e) {
+            return "user not found";
+        }
     }
 
     @GetMapping("admin/update/{username}")
